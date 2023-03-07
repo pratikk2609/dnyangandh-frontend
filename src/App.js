c96fe63756129a3252e51c1e3e18a841d81a31e9
pratikk2609 <pratikpawar2609@gmail.com>
2023-03-07 18:34:53 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn pravin
         </a>
       </header>
     </div>
