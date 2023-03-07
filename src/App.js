01fcd6898ddd2a735937905d4383c15251bc6d36
pratikk2609 <pratikpawar2609@gmail.com>
2023-03-07 20:17:18 +0530

    another build trigger

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Pravin Again
+          Learn Pravin Again with Pratik
         </a>
       </header>
     </div>
