8ef9e50cd0a410b017580a66d3421b0e34494779
pratikshatrixie <pratikshatrixie@gmail.com>
2023-04-15 09:56:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Dnyangandh Pratishtan Work 
+          Dnyangandh Pratishtan Pratiksha 
         </a>
       </header>
     </div>
