cba3aee6ab0ed7165e6a37bd802d0fbd05898310
pratikk2609 <pratikpawar2609@gmail.com>
2023-04-15 11:46:56 +0530

    new change

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -11,47 +11,47 @@ function Login() {
     const handleSubmit = (e) => {
     }
 
-return (
+    return (
 
-    <div className='App'>
-        <Fragment>
-            <div className="header">
-                <a href="#default" className="logo">Login Page</a>
-                <div className="header-right">
-                    <a href="/">Home</a>
-                    <a href="/register">Register</a>
-                    <a href="/login">Login</a>
-                </div>
-
-            </div>
-
-            <body>
-
-                <div className="col-md-5 no-gutters leftside">
-
-                    <Form className="d-grip gap-1" style={{ margin: "4rem" }}>
-                        <Form.Control type="text" placeholder="Enter Name" required
-                            onChange={(e) => setName(e.target.value)} ></Form.Control>
-                    </Form>
-
-                    <Form className="d-grip gap-1" style={{ margin: "4rem" }}>
-                        <Form.Control type="text" placeholder="Enter Email" required
-                            onChange={(e) => setEmail(e.target.value)} ></Form.Control>
-                    </Form>
-
-                    <Form className="d-grip gap-1" style={{ margin: "4rem" }}>
-                        <Form.Control type="text" placeholder="Enter Password" required
-                            onChange={(e) => setPassword(e.target.value)} ></Form.Control>
-                    </Form>
-
-                    <Button size='lg' onClick={(e) => handleSubmit(e)} type="submit">Submit</Button>
+        <div className='App'>
+            <Fragment>
+                <div className="header">
+                    <a href="#default" className="logo">Login Page</a>
+                    <div className="header-right">
+                        <a href="/">Home</a>
+                        <a href="/register">Register</a>
+                        <a href="/login">Login</a>
+                    </div>
 
                 </div>
 
-            </body>
-        </Fragment>
+                <body>
 
-    </div>
+                    <div className="col-md-5 no-gutters leftside">
+
+                        <Form className="d-grip gap-1" style={{ margin: "4rem" }}>
+                            <Form.Control type="text" placeholder="Enter Name" required
+                                onChange={(e) => setName(e.target.value)} ></Form.Control>
+                        </Form>
+
+                        <Form className="d-grip gap-1" style={{ margin: "4rem" }}>
+                            <Form.Control type="text" placeholder="Enter Email" required
+                                onChange={(e) => setEmail(e.target.value)} ></Form.Control>
+                        </Form>
+
+                        <Form className="d-grip gap-1" style={{ margin: "4rem" }}>
+                            <Form.Control type="text" placeholder="Enter Password" required
+                                onChange={(e) => setPassword(e.target.value)} ></Form.Control>
+                        </Form>
+
+                        <Button size='lg' onClick={(e) => handleSubmit(e)} type="submit">Submit</Button>
+
+                    </div>
+
+                </body>
+            </Fragment>
+
+        </div>
     )
 }
 
