7a4b64106a1230c4bfb868b3f449945fe6f6cf2e
pratikk2609 <pratikpawar2609@gmail.com>
2023-04-15 09:53:15 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Dnyangandh Pratishtan Website
+          Dnyangandh Pratishtan Work 
         </a>
       </header>
     </div>
