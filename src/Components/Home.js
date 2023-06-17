a693167664a69b9b7145ac44ef4de3ef4092d324
sailee2781 <saileekadam03@gmail.com>
2023-06-17 18:37:04 +0530

    updated home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,10 +1,11 @@
 import React, { Fragment } from "react"
 import "bootstrap/dist/css/bootstrap.min.css"
 import './App.css'
+import bg from '../images/bg.jpg'
+import './Home.css'
 import Footer from './Footer'
 
-function Home()
-{
+function Home() {
     return (
 
         <div className='App'>
@@ -17,9 +18,31 @@ function Home()
                         <a href="/scholarship">Scholarship</a>
                         <a href="/user/authenticate">Login</a>
                     </div>
-    
+
                 </div>
             </Fragment>
+            <div className="img-home">
+                <img src={bg} height="500px" width="100%" />
+            </div>
+            <div className="mission-container">
+                <div>
+                    <h3>Why we are here?</h3>
+                    <h6>How we are going to help u?</h6>
+                </div>
+                <div>
+                    <p>We as a Dnyangandh Team want to help students to identify their goals and achieve them. We will be providing necessary scholarships for them.</p>
+                </div>
+            </div>
+            <div className="mission-details">
+                <div className="mission-img">
+                    <img src="" alt="Images describing the same" />
+                </div>
+                <div className="mission-tagline">
+                    <h3>Events oraganised</h3>
+                    <h6>Literacy Campaigns going on</h6>
+                </div>
+            </div>
+            <Footer />
         </div>
     )
 }
